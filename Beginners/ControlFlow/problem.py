575d2b1d38e0e686fad83ab94f4c3f1e937ea668
chrissherfield <christopher.sherfield@gmail.com>
2018-04-11 18:19:01 +0100

    Completed control flow

--- Beginners/ControlFlow/problem.py
+++ Beginners/ControlFlow/problem.py
@@ -4,9 +4,12 @@ Write a function 'what_sign' which returns 'Positive' 'Zero' or 'Negative' when 
 
 
 def what_sign(n):
-    # Your code goes here
-    pass
-
+    if n < 0:
+        return "Negative"
+    elif n == 0:
+        return "Zero"
+    else:
+        return "Positive"
 
 def test_what_sign():
     assert what_sign(3) == 'Positive'
@@ -21,8 +24,18 @@ For numbers which are multiples of both three and five print "FizzBuzz".
 """
 
 def fizzbuzz():
-    # Your code goes here
-    pass
+    for i in range(1,101):
+        if i % 3 == 0 or i % 5 == 0:
+            if i%3 != 0:
+                print("Buzz")
+            elif i % 5 != 0:
+                print("Fizz")
+            else:
+                print("FizzBuzz")
+
+        else:
+            print(i)
+
 
 """
 Write a function which removes one or more indicies from a list.
@@ -36,8 +49,13 @@ the resulting list will be:
 """
 
 def remove_indices(mylist, idxs):
-    # Your code goes here
-    pass
+
+    li = []
+    for counter, item in enumerate(mylist):
+        if counter not in idxs:
+            li.append(item)
+
+    return li
 
 def test_remove_indices():
     assert remove_indices(["John", "Bob", "Charles", "Trev"], [0]) == ["Bob", "Charles", "Trev"]
@@ -54,12 +72,20 @@ def open_connection():
         raise ValueError('failed to connect')
     return True
 
-def connect():
+def connect(nretry = 100):
     # Your code to handle bad connection goes here
     # You might need to modify the arguments passed to connect() such as
     # how many times the caller wants to try to make a connection before
     # giving up.
-    return open_connection()
+    for i in range(nretry):
+
+        try:
+            if open_connection():
+                return True
+        except ValueError:
+            continue
+
+    return False
 
 def test_connect():
     for _i in range(100):
