61db391674dbab122de9e9b6ba0977a6cdb2944a
chrissherfield <christopher.sherfield@gmail.com>
2018-04-11 18:01:35 +0100

    Completed LengthClass

--- Beginners/ClassLength/Length.py
+++ Beginners/ClassLength/Length.py
@@ -37,22 +37,37 @@ import sys
 import pytest
 # Create a specialised exception called ExceptionLength that we can use on error.
 
+class ExceptionLength(Exception):
+        pass
+
+
 # Fill in the functions that Length needs.
 class Length(object):
     """Class that represents length. Constructor takes a number represents
     metres."""
+    CONVERSION_FACTOR = {
+        'feet': 1 / 0.3048,
+        'inch': 1000 / 25.4,
+        'mm': 1000,
+    }
     
     def __init__(self, theValue):
-        pass
+        self.value = theValue
         
     def __str__(self):
-        pass
+        return "Length: {0:.3f} (m)".format(self.value)
     
     def convert(self, uom):
         """Returns the value in the specified units of measure."""
-        pass
+        try:
+            return self.value * Length.CONVERSION_FACTOR[uom]
+        except KeyError:
+            raise ExceptionLength("Can not convert to: {0:s}".format(uom))
     
     # Create the appropriate function to add two Length classes together
+    def __add__(self, other):
+
+        return Length(self.value + other.value)
 
 
 def test_convert():
@@ -69,7 +84,7 @@ def test_convert_raises():
     length = Length(12.5)
     with pytest.raises(ExceptionLength) as err:
         length.convert('miles')
-    assert 'Can not convert to: miles' == err.value.message
+    assert "Can not convert to: miles" == err.value.message
 
 
 def test_add():
